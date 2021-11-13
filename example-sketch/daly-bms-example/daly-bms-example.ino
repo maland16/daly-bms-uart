eaa61dfeac00ec508c222145974f6ac00e124ead
Rocky <44615614+softwarecrash@users.noreply.github.com>
2021-11-13 18:30:06 +0100

    Update daly-bms-example.ino

--- example-sketch/daly-bms-example/daly-bms-example.ino
+++ example-sketch/daly-bms-example/daly-bms-example.ino
@@ -1,11 +1,15 @@
+#define BMS_SERIAL Serial1 //Set the serial port for communication with the Daly BMS
+#define DEBUG_SERIAL Serial //Set the Serial Debug port
+
 #include <Arduino.h>
 #include <daly-bms-uart.h> // This is where the library gets pulled in
 
 // Constructing the bms driver and passing in the Serial interface (which pins to use)
-Daly_BMS_UART bms(Serial1);
+
+Daly_BMS_UART bms(BMS_SERIAL);
 void setup() {
   // This is needed to print stuff to the serial monitor
-  Serial.begin(9600);
+  DEBUG_SERIAL.begin(9600);
 
   // This call sets up the driver
   bms.Init();
@@ -13,23 +17,18 @@ void setup() {
 
 void loop() {
 
-  // Grab those values from the BMS
-  //bms.getPackMeasurements();
-
+  // call to update the data from the bms
   bms.update();
 
 
   // And print them out!
-  Serial.println((String)bms.get.packVoltage+"V "+(String)bms.get.packCurrent+"I "+(String)bms.get.packSOC+"\% ");
-
-  // Now the same thing, but for temperature
-  //bms.getPackTemp();
-  Serial.println("Package Temperature: "+(String)bms.get.tempAverage);
-
-  // And again, for min/max cell voltages
-  //bms.getMinMaxCellVoltage();
-  Serial.println("Highest Cell Voltage Nr:"+(String)bms.get.maxCellVNum+" with voltage "+(String)(bms.get.maxCellmV / 1000));
-  Serial.println("Lowest Cell Voltage Nr:"+(String)bms.get.minCellVNum+" with voltage "+(String)(bms.get.minCellmV / 1000));
+  DEBUG_SERIAL.println((String)bms.get.packVoltage+"V "+(String)bms.get.packCurrent+"I "+(String)bms.get.packSOC+"\% ");
+  DEBUG_SERIAL.println("Package Temperature: "+(String)bms.get.tempAverage);
+  DEBUG_SERIAL.println("Highest Cell Voltage Nr:"+(String)bms.get.maxCellVNum+" with voltage "+(String)(bms.get.maxCellmV / 1000));
+  DEBUG_SERIAL.println("Lowest Cell Voltage Nr:"+(String)bms.get.minCellVNum+" with voltage "+(String)(bms.get.minCellmV / 1000));
+  DEBUG_SERIAL.println("Number of Cells: "+(String)bms.get.numberOfCells);
+  DEBUG_SERIAL.println("Numver of Temp Sensors: "+(String)bms.get.numOfTempSensors);
+  DEBUG_SERIAL.println("BMS Charge Discharge Cycles: "+(String)bms.get.bmsCycles);
  
 
 //Serial.print("\r");
