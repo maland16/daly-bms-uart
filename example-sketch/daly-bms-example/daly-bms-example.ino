0a9e2ac9291b72e568159f4630b06c8ad675b93c
Rocky <44615614+softwarecrash@users.noreply.github.com>
2022-04-06 17:52:11 +0200

    Add files via upload
    added cell balance and temperatur sensor arrays
    fix little bugs

--- example-sketch/daly-bms-example/daly-bms-example.ino
+++ example-sketch/daly-bms-example/daly-bms-example.ino
@@ -1,74 +1,43 @@
-#include <Arduino.h>
-
-#define BMS_SERIAL Serial1  //Set the serial port for communication with the Daly BMS
-#define DEBUG_SERIAL Serial //Set the Serial Debug port
-
-// NOTE: You must define and call DEBUG_SERIAL.begin(<baud rate>) in your setup() for this to work
-#define DALY_BMS_DEBUG // Uncomment the below #define to enable debugging print statements.
-
 #include <daly-bms-uart.h> // This is where the library gets pulled in
 
-//for testing, remove when working
-unsigned long previousMillis = 0;
-const long interval = 5000;
-
 // Constructing the bms driver and passing in the Serial interface (which pins to use)
-Daly_BMS_UART bms(BMS_SERIAL);
-void setup()
-{
+Daly_BMS_UART bms(Serial1);
+
+void setup() {
   // This is needed to print stuff to the serial monitor
-  DEBUG_SERIAL.begin(9600);
+  Serial.begin(115200);
 
   // This call sets up the driver
   bms.Init();
 }
 
-void loop()
-{
+void loop() {
 
-  // call to update the data from the bms
-  bms.update();
+  // Set up some variables to hold the values
+  float volts = 0;
+  float amps = 0;
+  float percentage = 0;
+
+  // Grab those values from the BMS
+  bms.getPackMeasurements(volts, amps, percentage);
 
   // And print them out!
-  DEBUG_SERIAL.println("basic BMS Data:              " + (String)bms.get.packVoltage + "V " + (String)bms.get.packCurrent + "I " + (String)bms.get.packSOC + "\% ");
-  DEBUG_SERIAL.println("Package Temperature:         " + (String)bms.get.tempAverage);
-  DEBUG_SERIAL.println("Highest Cell Voltage Nr:     " + (String)bms.get.maxCellVNum + " with voltage " + (String)(bms.get.maxCellmV / 1000));
-  DEBUG_SERIAL.println("Lowest Cell Voltage Nr:      " + (String)bms.get.minCellVNum + " with voltage " + (String)(bms.get.minCellmV / 1000));
-  DEBUG_SERIAL.println("Number of Cells:             " + (String)bms.get.numberOfCells);
-  DEBUG_SERIAL.println("Number of Temp Sensors:      " + (String)bms.get.numOfTempSensors);
-  DEBUG_SERIAL.println("BMS Chrg / Dischrg Cycles:   " + (String)bms.get.bmsCycles);
-  DEBUG_SERIAL.println("BMS HEartbeat:               " + (String)bms.get.bmsHeartBeat); //cycle 0-255
-  DEBUG_SERIAL.println("Discharge MOSFet Status:     " + (String)bms.get.disChargeFetState);
-  DEBUG_SERIAL.println("Charge MOSFet Status:        " + (String)bms.get.chargeFetState);
-  DEBUG_SERIAL.println("Remaining Capacity mAh:      " + (String)bms.get.resCapacitymAh);
+  Serial.printf("V: %4.1f, I: %4.1f, \%:%4.1f\n",volts, amps, percentage);
 
-  //for alarm flags - for all flags see the alarm struct in daly-bms-uart.h
-  DEBUG_SERIAL.println("Level one Cell V to High:    " + (String)bms.alarm.levelOneCellVoltageTooHigh);
+  // Now the same thing, but for temperature
+  int8_t temp = 0;
+  bms.getPackTemp(temp);
+  Serial.printf("Temp: %d\n",temp);
 
-/**
- * Advanced functions:
- * bms.setBmsReset(); //Reseting the BMS, after reboot the MOS Gates are enabled!
- * bms.SetMOSGate(true); Switches on the charge and discharge Gate
- * bms.setMOSGate(false); Switches off the charge and discharge Gate
- */
-  
+  // And again, for min/max cell voltages
+  float maxCellVoltage = 0;
+  float minCellVoltage = 0;
+  uint8_t maxCellNumber = 0;
+  uint8_t minCellNumber = 0;
+  bms.getMinMaxCellVoltage(minCellVoltage, minCellNumber, maxCellVoltage, maxCellNumber);
+  Serial.printf("Highest Cell Voltage: Cell #%d with voltage %4.3f\n",maxCellNumber,maxCellVoltage);
+  Serial.printf("Lowest Cell Voltage: Cell #%d with voltage %4.3f\n",minCellNumber,minCellVoltage);
 
-  
-/*
-  //Sample to switch on and off the MOS Gates function
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
-    if (bms.get.disChargeFetState)
-    {
-      bms.setMOSGate(false);
-    }
-    else
-    {
-      bms.setMOSGate(true);
-    }
-  }
-  */
- 
+  // Lets slow things down a bit...
+  delay(500);
 }
